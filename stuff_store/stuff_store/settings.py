dbbee0bb6eb69d39444bde05339dc99f79215430
Ammar sahyoun <ammarzgr@hotmail.com>
2021-08-17 23:01:40 +0200

    Create Product Model and db

--- stuff_store/stuff_store/settings.py
+++ stuff_store/stuff_store/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'debug_toolbar',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
     }
 }
 
