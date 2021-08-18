eb86f622ce75d4d955e691d924eaf7cd75bed930
Ammar sahyoun <ammarzgr@hotmail.com>
2021-08-18 21:53:30 +0200

    Add Category class

--- stuff_store/store/models.py
+++ stuff_store/store/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    stock_count = models.IntegerField()
+    name = models.CharField(max_length=100, help_text="Enter the full name!")
+    stock_count = models.IntegerField(help_text="The current items in the stock")
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(default="")
+    description = models.TextField(default="", blank=True)
+    sku = models.CharField(verbose_name="stock keeping unit", max_length=20,default="", unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.image
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.cat_name
+
+
 
